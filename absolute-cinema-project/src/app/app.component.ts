9c208aed1a051e49deaf42344d05f849ccd32cc2
Vinícius Luz Assunção <vinicius.luz.foto@gmail.com>
2024-11-29 18:01:55 -0300

    Fix: COrreção de arquivos com problema no NPM

--- absolute-cinema-project/src/app/app.component.ts
+++ absolute-cinema-project/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskbarComponent } from "./shared/components/taskbar/taskbar.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskbarComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
