e4fae7638f49e0a0b3986af571214113086ef109
Vinícius Luz Assunção <vinicius.luz.foto@gmail.com>
2024-11-30 02:57:28 -0300

    Feat: adiciona tailwind e bootstrap

--- absolute-cinema-project/src/app/app.component.ts
+++ absolute-cinema-project/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskbarComponent } from "./shared/components/taskbar/taskbar.component";
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskbarComponent, MatIconModule],
+  imports: [RouterOutlet, TaskbarComponent, MatIconModule, MatIconModule, MatButtonModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
